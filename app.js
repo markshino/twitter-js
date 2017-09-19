18ea84a642cd52e5bef737b6d85fb66b4da519c3
Daniel Bernard <danraybernard@gmail.com>
2017-09-19 15:21:15 -0400

    changes

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
 const routes = require('./routes');
-app.use('/', routes);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const app = express();
-// const logger = require('morgan');
-// const nunjucks = require('nunjucks');
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const nunjucks = require('nunjucks');
 
 // const locals = {
 //     title: 'An Example',
@@ -35,17 +13,14 @@ app.use('/', routes);
 //     ]
 // };
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 
-// app.set('view engine', 'html');
-// app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+app.use('/', routes);
+nunjucks.configure('views', {noCache: true});
 
-// nunjucks.configure('views', {noCache: true});
 
-// app.get('/', (req, res) => {
-//     res.render('index.html', locals);
-// });
-
-// app.listen(3000, function(){
-//     console.log('server listening');
-// });
+app.listen(3000, function(){
+    console.log('server listening');
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-const router = require('express').Router();
+const express =require('express');
+const router = express.Router();
 const tweetBank = require('../tweetbank');
+router.use(express.static('public'));
 
 router.get('/', (req, res) => {
     let tweets = tweetBank.list();
     res.render('index', {tweets: tweets});
 })
 
+router.get('/users/:name', (req, res) => {
+    let name = req.params.name;
+    let list = tweetBank.find( {name: name} )
+    res.render('index', {})
+})
+
+
+router.get('/stylesheets/style.css', (req, res) => {
+    res.sendFile('style.css');
+
+});
+
+
 module.exports = router;
+
